9da9e132ea28a238329360ad2695d761c67ae040
DESKTOP-UTMV71C\河边一瓶水 <2517099632@qq.com>
2018-11-10 20:42:34 +0800

    2018年11月 第十周 4-18 焦玉锋

--- 4.18/main.cpp
+++ 4.18/main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
 int main()
 {
     int a=1;
-    cout<<"N\t"<<setw(10)<<"10*N\t"<<setw(10)<<"100*N\t"<<setw(10)<<"1000*N\t"<<endl;
+    cout<<"N\t"<<"10*N\t"<<"100*N\t"<<"1000*N\t"<<endl<<endl;
     while(a<6)
     {
-        cout<<endl;
         cout<<a<<"\t"<<10*a<<"\t"<<100*a<<"\t"<<1000*a<<endl;
         a+=1;
     }
